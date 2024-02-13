609ee0049822c4f5b14de49e520cb252332c62b1
Aydar <mr.ivaoimop@mail.ru>
2024-02-13 13:21:06 +0300

    SI-7898 Добавлен inernal, исправлен useCase

--- data/src/main/java/com/example/androidjetpack/data/di/MoviesDiModule.kt
+++ data/src/main/java/com/example/androidjetpack/data/di/MoviesDiModule.kt
@@ -1,7 +1,11 @@
 package com.example.androidjetpack.data.di
 
 import com.example.androidjetpack.data.movie.MovieApi
+import com.example.androidjetpack.data.repository.FavoriteMoviesRepositoryImpl
 import com.example.androidjetpack.data.repository.MovieRepositoryImpl
+import com.example.androidjetpack.data.room.AppDatabase
+import com.example.androidjetpack.data.movie.FavoriteMoviesDao
+import com.example.androidjetpack.domain.repository.FavoriteMoviesRepository
 import com.example.androidjetpack.domain.repository.MovieRepository
 import dagger.Module
 import dagger.Provides
@@ -24,5 +28,16 @@ internal object MoviesDiModule {
     @Singleton
     fun provideMovieRepository(movieApi: MovieApi): MovieRepository {
         return MovieRepositoryImpl(movieApi)
+    }
+
+    @Provides
+    fun provideFavoriteMoviesDao(database: AppDatabase): FavoriteMoviesDao {
+        return database.favoriteMoviesDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFavoriteMoviesRepository(favoriteMoviesDao: FavoriteMoviesDao): FavoriteMoviesRepository {
+        return FavoriteMoviesRepositoryImpl(favoriteMoviesDao)
     }
 }

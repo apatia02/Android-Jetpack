b2984b1e2f97e3c7167943f7725ff6e8176ebdac
Aydar <mr.ivaoimop@mail.ru>
2024-02-20 11:19:15 +0300

    SI-7902 добавлено новое состояние, загрузка в виде прогресс бара, изменена логика горизонтального прогрес бара.

--- presentation/src/main/java/com/example/androidjetpack/presentation/MainViewModel.kt
+++ presentation/src/main/java/com/example/androidjetpack/presentation/MainViewModel.kt
@@ -5,16 +5,16 @@ import androidx.lifecycle.viewModelScope
 import com.example.androidjetpack.domain.EMPTY_STRING
 import com.example.androidjetpack.domain.entity.MovieList
 import com.example.androidjetpack.domain.use_case.MoviesUseCase
-import com.example.androidjetpack.presentation.UiConstants.PROGRESS_FINISH
-import com.example.androidjetpack.presentation.UiConstants.PROGRESS_STEP
-import com.example.androidjetpack.presentation.loading_state.LoadViewState
+import com.example.androidjetpack.presentation.loading_state.LoadViewState.ERROR
+import com.example.androidjetpack.presentation.loading_state.LoadViewState.MAIN_LOADING
+import com.example.androidjetpack.presentation.loading_state.LoadViewState.NONE
+import com.example.androidjetpack.presentation.loading_state.LoadViewState.TRANSPARENT_LOADING
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,19 +22,16 @@ class MainViewModel @Inject constructor(
     private val moviesUseCase: MoviesUseCase
 ) : ViewModel() {
 
-    private val _currentState = MutableStateFlow(LoadViewState.NONE)
+    private val _currentState = MutableStateFlow(NONE)
     val currentState = _currentState.asStateFlow()
 
     private val _movies = MutableStateFlow(MovieList())
     val movies = _movies.asStateFlow()
 
-    private val _progressRequest = MutableStateFlow(0)
-    val progressRequest = _progressRequest.asStateFlow()
+    private val _snackError = MutableSharedFlow<Unit>()
+    val snackError = _snackError.asSharedFlow()
 
-    private val _snackError = MutableStateFlow(false)
-    val snackError = _snackError.asStateFlow()
-
-    val hasData: Boolean
+    private val hasData: Boolean
         get() = _movies.value.listMovie.isNotEmpty()
 
     init {
@@ -44,45 +41,28 @@ class MainViewModel @Inject constructor(
     }
 
     suspend fun getMovies(query: String) {
-        if (hasData) {
-            getMoviesHasData(query)
-        } else getMoviesHasNotData(query)
-    }
-
-    private suspend fun getMoviesHasNotData(query: String) {
         try {
-            _currentState.value = LoadViewState.LOADING
+            if (hasData) {
+                _currentState.value = TRANSPARENT_LOADING
+            } else {
+                _currentState.value = MAIN_LOADING
+            }
             _movies.value = moviesUseCase.getMovies(query)
-        } catch (e: Exception) {
-            _currentState.value = LoadViewState.ERROR
-        } finally {
-            if (_currentState.value != LoadViewState.ERROR) {
-                _currentState.value = LoadViewState.NONE
+            if (_currentState.value != ERROR) {
+                _currentState.value = NONE
             }
-        }
-    }
-
-    private suspend fun getMoviesHasData(query: String) {
-        try {
-            startProgress()
-            withContext(Dispatchers.IO) {
-                _movies.value = moviesUseCase.getMovies(query)
-            }
-            _progressRequest.value = PROGRESS_FINISH
         } catch (e: Exception) {
-            showSnackError()
-        }
-    }
-
-    private suspend fun startProgress() {
-        for (progress in 0 until PROGRESS_FINISH step PROGRESS_STEP) {
-            _progressRequest.value = progress
-            delay(100)
+            if (hasData) {
+                showSnackError()
+            } else {
+                _currentState.value = ERROR
+            }
         }
     }
 
     private fun showSnackError() {
-        _snackError.value = true
-        _snackError.value = false
+        viewModelScope.launch {
+            _snackError.emit(Unit)
+        }
     }
 }
--- presentation/src/main/java/com/example/androidjetpack/presentation/UiConstants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.androidjetpack.presentation
-
-object UiConstants {
-    const val PROGRESS_STEP = 10
-    const val PROGRESS_FINISH = 100
-}
